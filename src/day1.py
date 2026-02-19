81971cff148b3c58d5d64fd07c220ca65dfe01d9
Tom Smilack <tom@smilack.net>
2026-02-18 22:21:02 -0600

    Improve style/naming and solve part 2

--- src/day1.py
+++ src/day1.py
@@ -1,25 +1,7 @@
 import sys
+from functools import reduce
 
-def toInt(s):
-    return int(s[1:]) * (1 if s[0] == 'R' else -1)
-
-def runningTotal(l):
-    current = 50
-    return [current := (current + n) % 100 for n in l]
-
-def part1(turns):
-    totals = runningTotal(turns)
-    return totals.count(0)
-
-def part2(turns):
-    return 0
-
-if __name__ == "__main__":
-    if len(sys.argv) >= 2:
-        with open(sys.argv[1]) as f:
-            puzzleInput = f.read()
-    else:
-        puzzleInput = """L68
+EXAMPLE = """L68
 L30
 R48
 L5
@@ -30,7 +12,48 @@ L99
 R14
 L82"""
 
-    turns = [toInt(s) for s in puzzleInput.split()]
 
-    print("Part one:", part1(turns)) 
-    print("Part two:", part2(turns))
+def rotation_to_int(rotation: str) -> int:
+    return int(rotation[1:]) * (1 if rotation[0] == "R" else -1)
+
+
+def stops(turns: list[int]) -> list[int]:
+    current = 50
+    return [current := (current + n) % 100 for n in turns]
+
+
+def stops_at_zero(turns: list[int]) -> int:
+    return stops(turns).count(0)
+
+
+def turns_through_zero(turns: list[int]) -> int:
+    _, zeroes = reduce(rotate_through, turns, (50, 0))
+    return zeroes
+
+
+def rotate_through(acc: tuple[int, int], turn) -> tuple[int, int]:
+    """Do rotation and count number of passes through zero."""
+    current, zeroes = acc
+    moved = current + turn
+
+    # crossed 0
+    if current > 0 and moved <= 0:
+        zeroes += 1
+
+    # crossed a multiple of 100
+    zeroes = int(abs(moved) / 100)
+
+    return (moved % 100, zeroes)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1]) as f:
+            rotations = f.read()
+    else:
+        rotations = EXAMPLE
+
+    turns = [rotation_to_int(s) for s in rotations.split()]
+
+    print("Part one:", stops_at_zero(turns))
+    print("Part two:", turns_through_zero(turns))
