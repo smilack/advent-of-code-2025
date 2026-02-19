66d4ad1331117295469a04f33b726417e7b16048
Tom Smilack <tom@smilack.net>
2026-02-19 00:04:54 -0600

    Create solution template

---
+++ src/template.py
@@ -0,0 +1,10 @@
+import utils
+
+if __name__ == "__main__":
+    puzzle_input = utils.read_lines()
+    # puzzle_input = utils.read_csv()
+    # puzzle_input = utils.read_raw()
+
+    print("Part 1:")
+
+    print("Part 2:")
