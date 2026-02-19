1e3f517ddacb2ca8b1b864b8e8817c184bff70a6
Tom Smilack <tom@smilack.net>
2026-02-19 13:50:42 -0600

    Day 5 part 2 solution. Turns out min(range_) and range_.start do NOT have the same performance

--- src/day5.py
+++ src/day5.py
@@ -1,7 +1,5 @@
 import utils
 
-from functools import reduce
-
 
 def make_range(s):
     low, high = map(int, s.split("-"))
@@ -18,6 +16,54 @@ def get_full_range(ranges):
     return range(lowest, highest)
 
 
+def add_range(ranges, new_range):
+    """Add a range to a list of ranges, merging it into an existing range if possible.
+
+    ranges is assumed to be sorted and new_range must sort >= ranges[-1].
+    """
+    if len(ranges) == 0:
+        ranges.append(new_range)
+    else:
+        ranges.extend(merge_ranges(ranges.pop(), new_range))
+
+
+def sort_ranges(ranges):
+    with_metadata = [(r.start, r.stop, i, r) for (i, r) in enumerate(ranges)]
+    with_metadata.sort()
+    return [range_ for (_, _, _, range_) in with_metadata]
+
+
+def merge_ranges(a: range, b: range) -> list[range]:
+    """Combine two ranges, if possible, otherwise leave separate. Return a list
+    of either one or two ranges.
+
+    a is assumed to be sorted before b (sorting by min asc, max asc).
+
+    |---a---|
+    |---b---|
+
+    |---a---|
+    |---b------|
+
+    |---a---|
+       |---b---|
+
+    |-----a-----|
+      |---b---|
+    """
+    if b.start in a:
+        return [range(a.start, max(a.stop, b.stop))]
+    else:
+        return [a, b]
+
+
+def merge_all_ranges(sorted_ranges):
+    merged_ranges = []
+    for r in sorted_ranges:
+        add_range(merged_ranges, r)
+    return merged_ranges
+
+
 if __name__ == "__main__":
     puzzle_input = utils.read_lines()
 
@@ -26,8 +72,7 @@ if __name__ == "__main__":
 
     print("Part 1:", count_fresh(available_ids, fresh_ranges))
 
-    full_range = get_full_range(fresh_ranges)
-    num_fresh = count_fresh(full_range, fresh_ranges)
-    # print("Full range length:", len(full_range))
-    # print("Number of ranges:", len(fresh_ranges))
-    print("Part 2:", num_fresh)
+    sorted = sort_ranges(fresh_ranges)
+    super_range = merge_all_ranges(sorted)
+    total_fresh = sum([len(range_) for range_ in super_range])
+    print("Part 2:", total_fresh)
