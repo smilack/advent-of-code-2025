a0ddbaff35e7d138c1ae5326365505331626bfdd
Tom Smilack <tom@smilack.net>
2026-02-19 22:15:03 -0600

    Day 6 part 2 solution

--- src/day6.py
+++ src/day6.py
@@ -13,8 +13,8 @@ def solve_problem(problem):
 
     For example, solve_problem(["123", "45", "6", "*"]) == 33210.
     """
-    operands = [int(n) for n in problem[:-1]]
-    operator = operators[problem[-1]]
+    operands = [int(n.strip()) for n in problem[:-1]]
+    operator = operators[problem[-1].strip()]
     return reduce(operator, operands)
 
 
@@ -32,7 +32,7 @@ def transpose_numbers(problem):
         98
         ["328", "64", "98"] becomes ["8", "248", "369"]
     """
-    transposed = np.transpose([number.split() for number in problem])
+    transposed = np.transpose([list(number) for number in problem])
     converted = ["".join(line).strip() for line in transposed]
     return converted
 
@@ -42,37 +42,44 @@ def solve(problems):
 
 
 if __name__ == "__main__":
-    # It turns out the vertical alignment matters, so I can't strip the whitespace this early.
-    puzzle_input = utils.read_table()
-    problems = np.transpose(puzzle_input)
-    print("Part 1:", solve(problems))
-
     puzzle_input = utils.read_lines()
 
+    # The operator is always aligned with the left column:
+    # 123 328  51 64
+    #  45 64  387 23
+    #   6 98  215 314
+    # *   +   *   +
+    # abcdefghijklmnop
+
+    # [a, e, i, m]
     operator_places = [
         match.start() for match in re.finditer(r"\*|\+", puzzle_input[-1])
     ]
 
-    # The operator is always aligned with the left column:
-    # 123
-    #  45
-    #   6
-    # *
+    # [(a, e-1), (e, i-1), (i, m-1)]
+    # [(a, d), (e, h), (i, l)]
     column_bounds = [
         (operator_places[i - 1], operator_places[i] - 1)
         for i in range(1, len(operator_places))
     ]
+    # (m, p)
     column_bounds.append((operator_places[-1], len(puzzle_input[-1])))
 
-    operator_columns = [puzzle_input[-1][start:stop] for (start, stop) in column_bounds]
+    # operator_columns = [puzzle_input[-1][start:stop] for (start, stop) in column_bounds]
     columns = [
         [line[start:stop] for (start, stop) in column_bounds] for line in puzzle_input
     ]
 
-    print(puzzle_input[-1])
-    print(operator_places)
-    print(column_bounds)
-    print(columns)
+    # print(puzzle_input[-1])
+    # print(operator_places)
+    # print(column_bounds)
+    # print(columns)
 
-    # part_2_problems = [transpose_numbers(p[:-1]) + [p[-1]] for p in problems]
-    # print("Part 2:", solve(part_2_problems))
+    problems = np.transpose(columns)
+    print("Part 1:", solve(problems))
+
+    [print(p) for p in problems]
+
+    part_2_problems = [transpose_numbers(p[:-1]) + [p[-1]] for p in problems]
+    [print(p) for p in part_2_problems]
+    print("Part 2:", solve(part_2_problems))
