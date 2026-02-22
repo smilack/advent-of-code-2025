f51f8f8ce60e0368a188805411afcead42e36e17
Tom Smilack <tom@smilack.net>
2026-02-22 13:19:18 -0600

    Clean up day 11 solutions

--- src/day11.py
+++ src/day11.py
@@ -1,9 +1,6 @@
 from functools import cache
 from re import findall
 import utils
-from itertools import count
-
-counter = count()
 
 
 def parse_device(s):
@@ -11,29 +8,21 @@ def parse_device(s):
     return (matches[0], matches[1:])
 
 
-def find_paths(devices, device, path=[], depth=1, visits=[]):
+def find_paths(devices, device, path=[], depth=1):
     if depth > len(devices):
         print("Potential cycle detected:", path)
 
     if device == "out":
-        if all([v in path for v in visits]):
-            print(f"{next(counter)} Valid path at depth", depth)
-            # print(path)
-            return 1
-        else:
-            print(f"{next(counter)} Invalid path at depth", depth)
-            return 0
+        return 1
     else:
         return sum(
-            [
-                find_paths(devices, d, path + [d], depth + 1, visits)
-                for d in devices[device]
-            ]
+            [find_paths(devices, d, path + [d], depth + 1) for d in devices[device]]
         )
 
 
 @cache
-def find_paths_good(device, passed_fft=False, passed_dac=False):
+# By not including the devices dict, all the arguments (str, bool, bool) are hashable, so the result can be cached
+def find_paths_hashable(device, passed_fft=False, passed_dac=False):
     if device == "out":
         if passed_dac and passed_fft:
             return 1
@@ -42,7 +31,9 @@ def find_paths_good(device, passed_fft=False, passed_dac=False):
     else:
         return sum(
             [
-                find_paths_good(d, passed_fft or d == "fft", passed_dac or d == "dac")
+                find_paths_hashable(
+                    d, passed_fft or d == "fft", passed_dac or d == "dac"
+                )
                 for d in devices[device]
             ]
         )
@@ -51,15 +42,10 @@ def find_paths_good(device, passed_fft=False, passed_dac=False):
 if __name__ == "__main__":
     puzzle_input = utils.read_lines()
 
-    global devices
     devices = dict([parse_device(line) for line in puzzle_input])
-    # print(devices)
 
     if utils.input_type() in ["example", "real"]:
-        paths = find_paths(devices, "you")
-        print("Part 1:", paths)
+        print("Part 1:", find_paths(devices, "you"))
 
     if utils.input_type() in ["example2", "real"]:
-        # paths2 = find_paths(devices, "svr", visits=["fft", "dac"])
-        paths2 = find_paths_good("svr")
-        print("Part 2:", paths2)
+        print("Part 2:", find_paths_hashable("svr"))
