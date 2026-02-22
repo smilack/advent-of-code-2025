ea4aabd9cae2c1ae7bec30a0df4cec6baed1eb35
Tom Smilack <tom@smilack.net>
2026-02-22 17:41:34 -0600

    Clean up/comment on day 12

--- src/day12.py
+++ src/day12.py
@@ -12,21 +12,20 @@ if __name__ == "__main__":
 
     present_areas = [area(p) for _, p in sorted(tree["presents"].items())]
 
-    ub = 0
+    # I thought this was just going to be a naive upper bound because the example input has a case where there is "enough" area for the presents, but they don't all fit.
+    # But it turns out, for the real input, the naive solution worked!
+
+    upper_bound = 0
     for i, ((w, h), req) in enumerate(tree["regions"]):
         ar = w * h
         total_area = sum([req[i] * present_areas[i] for i in range(len(req))])
         if total_area < ar:
-            ub += 1
+            upper_bound += 1
             possible = "✅"
         else:
-            possible = "✗"
+            possible = "❌"
         print(
             f"Region {i} area: {ar}. Total present area: {total_area}. Solution possible? {possible}"
         )
 
-    print("Upper bound:", ub)
-
-    print("Part 1:")
-
-    print("Part 2:")
+    print("Upper bound:", upper_bound)
