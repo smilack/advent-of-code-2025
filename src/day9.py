726c8161beb0bcfff0171b77445315e82c419739
Tom Smilack <tom@smilack.net>
2026-02-21 22:52:47 -0600

    Day 9 part 2: create path between red tiles and determine which are inside or outside

--- src/day9.py
+++ src/day9.py
@@ -1,19 +1,68 @@
+from itertools import pairwise, cycle
 import utils
 
+BLACK = "⬛"
+BLUE = "🟦"
 GREEN = "🟩"
 RED = "🟥"
+YELLOW = "🟨"
 
 
-def draw_grid(red_tiles):
+def steps(a, b):
+    if a == b:
+        return [a]
+    else:
+        return range(a, b, (b - a) // abs(b - a))
+
+
+def repeating_zip(a, b):
+    if len(a) > len(b):
+        return zip(a, cycle(b))
+    elif len(b) > len(a):
+        return zip(cycle(a), b)
+    else:
+        return zip(a, b)
+
+
+def get_path(a, b):
+    (x1, y1), (x2, y2) = a, b
+    return list(repeating_zip(steps(x1, x2), steps(y1, y2)))
+
+
+def is_edge(s):
+    return s in [RED, GREEN]
+
+
+def is_inside(s):
+    return s in [RED, GREEN, YELLOW]
+
+
+def evaluate_grid(red_tiles):
     xs = [p[0] for p in red_tiles]
     ys = [p[1] for p in red_tiles]
 
-    tiles = [[GREEN for _ in range(max(xs) + 1)] for _ in range(max(ys) + 1)]
+    tiles = [[BLACK for _ in range(max(xs) + 1)] for _ in range(max(ys) + 1)]
 
-    for x, y in red_tiles:
-        tiles[y][x] = RED
+    for (x1, y1), end in pairwise(red_tiles + [red_tiles[0]]):
+        tiles[y1][x1] = RED
+        for x2, y2 in get_path((x1, y1), end)[1:]:
+            tiles[y2][x2] = GREEN
 
-    print("\n".join(map("".join, tiles)))
+    for line in tiles:
+        inside = False
+        on_edge = False
+        for i in range(len(line)):
+            if is_edge(line[i]):
+                on_edge = True
+            else:
+                if on_edge:
+                    on_edge = False
+                    inside = not inside
+
+                if inside:
+                    line[i] = YELLOW
+
+    return tiles
 
 
 def difference(a, b):
@@ -25,9 +74,11 @@ if __name__ == "__main__":
 
     red_tiles = [tuple(map(int, line.split(","))) for line in puzzle_input]
 
+    tiles = evaluate_grid(red_tiles)
+
     # the real grid is almost 10,000 x 10,000
     if utils.input_type() == "example":
-        draw_grid(red_tiles)
+        print("\n".join(map("".join, tiles)))
 
     combinations = [(a, b) for i, a in enumerate(red_tiles) for b in red_tiles[:i]]
     areas = [
